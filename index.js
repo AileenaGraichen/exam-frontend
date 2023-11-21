438acce23fc06d3ae7c357396e0a07bbf45b608e
laura ramgil <lauraramgil@hotmail.com>
2023-11-21 12:24:20 +0100

    setting up manage accounts
    starting implementation for managing accounts

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import "https://unpkg.com/navigo"; //Will create the global Navigo object used below
 import "https://cdnjs.cloudflare.com/ajax/libs/dompurify/2.4.0/purify.min.js";
-
+//import "./navigo_EditedByLars.js";
 import { setActiveLink, renderHtml, loadHtml } from "./utils.js";
-
 //Add Init methods here
 import { initLogin } from "./pages/login/login.js";
 import { initManageAccounts } from "./pages/manageAccounts/accounts.js";
 
 window.addEventListener("load", async () => {
   //Load html templates here, for navigo
+
+  const templateLogin = await loadHtml("./pages/login/login.html");
   const templateAccounts = await loadHtml(
     "./pages/manageAccounts/accounts.html"
   );
-  const templateBooks = await loadHtml("./pages/login/login.html");
+  const templateHome = await loadHtml("./pages/home/home.html");
 
   const router = new Navigo("/", { hash: true });
   window.router = router;
@@ -24,22 +25,16 @@ window.addEventListener("load", async () => {
       },
     })
     .on({
-      //For very simple "templates", you can just insert your HTML directly like below
       "/": () => renderHtml(templateHome, "content"),
 
-      "/books": (match) => {
-        renderHtml(templateBooks, "content");
-        initLogin();
-      },
-
-      "/books-no-pagination": (match) => {
-        renderHtml(templateBooksNoPagination, "content");
-        initBooksNoPagination();
-      },
-
-      "/manage/accounts": (match) => {
+      "/accounts": () => {
         renderHtml(templateAccounts, "content");
         initManageAccounts();
+      },
+
+      "/login": () => {
+        renderHtml(templateLogin, "content");
+        initLogin();
       },
     })
     .notFound(
--- pages/manageAccounts/accounts.js
+++ pages/manageAccounts/accounts.js
@@ -1,5 +1,5 @@
 import { API_URL } from "../../settings.js";
-const URL = API_URL + "/api/user-with-role";
+const URL = API_URL + "/user-with-role";
 import {
   makeOptions,
   sanitizeStringWithTableRows,
@@ -8,24 +8,70 @@ import {
 
 export async function initManageAccounts() {
   renderAccounts();
+  document
+    .getElementById("create-account-btn")
+    .addEventListener("click", createAccount);
 }
 
 async function renderAccounts() {
   try {
-    const accounts = await fetch(URL, makeOptions("GET", null, false)).then(
+    // TODO change back to admin only - true
+    let accounts = await fetch(URL, makeOptions("GET", null, false)).then(
       handleHttpErrors
     );
 
-    const accountTable = accounts
+    console.log(accounts);
+
+    const accountDivs = accounts
       .map((account) => {
         return `<div>
-            <h1>${account.username}</h1>
-            </div>`;
+            <p>${account.userName}</p>
+        </div>`;
       })
       .join("");
 
-    document.getElementById("accounts-outerbox").innerHTML = accountTable;
+    document.getElementById("accounts-outerbox").innerHTML = accountDivs;
   } catch (err) {
+    //TODO Handle errors correctly
     console.error("Could not fetch accounts from server: " + err);
+  }
+}
+
+async function createAccount() {
+  const username = document.getElementById("create-username").value;
+  const email = document.getElementById("create-email").value;
+  const password = document.getElementById("create-password").value;
+
+  const createAccountBody = {
+    username: username,
+    email: email,
+    password: password,
+  };
+
+  try {
+    // TODO change back to admin only - true
+    await fetch(URL, makeOptions("patch", createAccountBody, false)).then(
+      handleHttpErrors
+    );
+    addRoleToAccount(username);
+    renderAccounts();
+  } catch (err) {
+    //TODO; Handle errors correctly
+    console.error("Could not create new account: " + err);
+  }
+}
+
+async function addRoleToAccount(username) {
+  const role = document.getElementById("add-role").value;
+
+  try {
+    // TODO change back to admin only - true
+    await fetch(
+      `${URL}/add-role/${username}/${role}`,
+      makeOptions("patch", null, false)
+    ).then(handleHttpErrors);
+  } catch (err) {
+    //TODO Handle errors correctly
+    console.error("Could not add role to account");
   }
 }
