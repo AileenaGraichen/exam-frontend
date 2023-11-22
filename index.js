ce6dc0108ba550ad4835b7773649f2f0dbfde7b2
laura ramgil <lauraramgil@hotmail.com>
2023-11-22 09:01:02 +0100

    Implement account registration
    implementation of the account registration for ADMIN role ony

--- index.js
+++ index.js
@@ -1,40 +1,23 @@
-<<<<<<< HEAD
 import "https://unpkg.com/navigo"; //Will create the global Navigo object used below
 import "https://cdnjs.cloudflare.com/ajax/libs/dompurify/2.4.0/purify.min.js";
 //import "./navigo_EditedByLars.js";
 import { setActiveLink, renderHtml, loadHtml } from "./utils.js";
-=======
-
-//import "https://unpkg.com/navigo"  //Will create the global Navigo object used below
-import "https://cdnjs.cloudflare.com/ajax/libs/dompurify/2.4.0/purify.min.js"
-import "./navigo_EditedByLars.js";
-import {setActiveLink, renderHtml, loadHtml} from "./utils.js"
->>>>>>> 2e86199eef9394a6e3f5126d82696418d429b0b2
 //Add Init methods here
-import {initLogin} from "./pages/login/login.js";
+import { initLogin } from "./pages/login/login.js";
 import { initManageAccounts } from "./pages/manageAccounts/accounts.js";
-
 
 window.addEventListener("load", async () => {
   //Load html templates here, for navigo
-<<<<<<< HEAD
 
   const templateLogin = await loadHtml("./pages/login/login.html");
+  const templateHome = await loadHtml("./pages/home/home.html");
   const templateAccounts = await loadHtml(
     "./pages/manageAccounts/accounts.html"
   );
-  const templateHome = await loadHtml("./pages/home/home.html");
-=======
-  
-  const templateLogin = await loadHtml("./pages/login/login.html")
-  const templateHome = await loadHtml("./pages/home/home.html")
-  const templateAccounts = await loadHtml("./pages/manageAccounts/accounts.html");
->>>>>>> 2e86199eef9394a6e3f5126d82696418d429b0b2
 
   const router = new Navigo("/", { hash: true });
   window.router = router;
   router
-<<<<<<< HEAD
     .hooks({
       before(done, match) {
         setActiveLink("menu", match.url);
@@ -60,30 +43,6 @@ window.addEventListener("load", async () => {
           "<h2>404 - Page not found</h2>")
     )
     .resolve();
-=======
-      .hooks({
-        before(done, match) {
-          setActiveLink("menu", match.url)
-          done()
-        }
-      })
-      .on({
-        "/": () => 
-        renderHtml(templateHome, "content"),
-
-        "/manage/accounts": (match) => {
-        renderHtml(templateAccounts, "content");
-        initManageAccounts();
-        },
-
-        "/login": () => {
-          renderHtml(templateLogin, "content");
-          initLogin();
-        }
-      })
-      .notFound(() => document.getElementById("content").innerHTML ="<h2>404 - Page not found</h2>")
-      .resolve()
->>>>>>> 2e86199eef9394a6e3f5126d82696418d429b0b2
 });
 
 window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
--- pages/manageAccounts/accounts.js
+++ pages/manageAccounts/accounts.js
@@ -11,26 +11,43 @@ export async function initManageAccounts() {
   document
     .getElementById("create-account-btn")
     .addEventListener("click", createAccount);
+  document
+    .getElementById("tbl-body")
+    .addEventListener("onclick", showAccountDetails);
+  // Get the modal
+  document.getElementById("account-modal").addEventListener("click", () => {});
 }
 
 async function renderAccounts() {
+  let accountIds = [];
   try {
-    // TODO change back to admin only - true
-    let accounts = await fetch(URL, makeOptions("GET", null, false)).then(
+    let accounts = await fetch(URL, makeOptions("GET", null, true)).then(
       handleHttpErrors
     );
 
     console.log(accounts);
 
-    const accountDivs = accounts
+    const accountRowsArray = accounts
       .map((account) => {
-        return `<div>
-            <p>${account.userName}</p>
-        </div>`;
+        accountIds.push(account.userName);
+        return ` <tr>                                
+        <td>${account.userName} </td>              
+        <td>${account.email} </td>                     
+        <td>${account.roleNames} </td>
+        <td>
+        <button id="account-btn_manage_${account.userName}" type="button"  class="btn btn-sm btn-primary">Manage</button> 
+        </td>      
+      </tr>`;
       })
       .join("");
 
-    document.getElementById("accounts-outerbox").innerHTML = accountDivs;
+    document.getElementById("tbl-body").innerHTML =
+      sanitizeStringWithTableRows(accountRowsArray);
+    accountIds.forEach((id) => {
+      document
+        .getElementById("account-btn_manage_" + id)
+        .addEventListener("click", showDetailsModal(id));
+    });
   } catch (err) {
     //TODO Handle errors correctly
     console.error("Could not fetch accounts from server: " + err);
@@ -49,12 +66,10 @@ async function createAccount() {
   };
 
   try {
-    // TODO change back to admin only - true
-    await fetch(URL, makeOptions("patch", createAccountBody, false)).then(
+    await fetch(URL, makeOptions("POST", createAccountBody, true)).then(
       handleHttpErrors
     );
     addRoleToAccount(username);
-    renderAccounts();
   } catch (err) {
     //TODO; Handle errors correctly
     console.error("Could not create new account: " + err);
@@ -65,13 +80,66 @@ async function addRoleToAccount(username) {
   const role = document.getElementById("add-role").value;
 
   try {
-    // TODO change back to admin only - true
     await fetch(
       `${URL}/add-role/${username}/${role}`,
-      makeOptions("patch", null, false)
+      makeOptions("PATCH", null, true)
     ).then(handleHttpErrors);
+    renderAccounts();
   } catch (err) {
     //TODO Handle errors correctly
     console.error("Could not add role to account");
   }
 }
+
+async function showAccountDetails(evt) {
+  const target = evt.target;
+  if (!target.id.startsWith("row-btn_")) {
+    return;
+  }
+
+  const parts = target.id.split("_");
+  const id = parts[2];
+  const btnAction = parts[1];
+  if (btnAction === "manage") {
+    showDetailsModal(id);
+  }
+}
+
+async function deleteAccount(evt) {
+  try {
+    await fetch(
+      `${URL}/delete-user/${username}`,
+      makeOptions("DELETE", null, true)
+    ).then(handleHttpErrors);
+  } catch (err) {
+    //TODO; Handle errors correctly
+    console.error("Could not delete account: " + err);
+  }
+}
+
+function showDetailsModal(id) {
+  document.getElementById("account-modal").style.display = "block";
+}
+
+/* // Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}; */
